1934ca3f67903abb53bb57bdbda62369dc6e35f0
Sirojiddin0929 <sirojiddinoyosboyev@gmail.com>
2025-11-16 17:42:55 +0500

    feat: all book files are created

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import {Router} from "express"
 import { authRouter } from "./auth.routes.js"
 import { userRouter } from "./user.routes.js"
 import { authorRouter } from "./author.routes.js"
+import { bookRouter } from "./book.routes.js"
 
 const MainRouter=Router()
 
 MainRouter.use("/auth",authRouter)
 MainRouter.use("/user",userRouter)
 MainRouter.use("/author",authorRouter)
+MainRouter.use("/book",bookRouter)
 
 export default MainRouter
--- src/validations/book.validation.js
+++ src/validations/book.validation.js
@@ -1,19 +1,19 @@
 import Joi from "joi";
 
 export const createBookSchema = Joi.object({
-  title: Joi.string().min(2).max(255).required(),
-  isbn: Joi.string().min(10).max(13).required(),
+  title: Joi.string().required(),
+  isbn: Joi.string().required(),
   authorId: Joi.string().uuid().required(),
-  category: Joi.string().min(2).max(100).required(),
-  publicationDate: Joi.date().required(),
-  totalCopies: Joi.number().integer().min(1).required()
+  category: Joi.string().required(),
+  publicationDate: Joi.date().iso().required(),
+  totalCopies: Joi.number().integer().min(1).required(),
 });
 
 export const updateBookSchema = Joi.object({
-  title: Joi.string().min(2).max(255),
-  isbn: Joi.string().min(10).max(13),
+  title: Joi.string(),
+  isbn: Joi.string(),
   authorId: Joi.string().uuid(),
-  category: Joi.string().min(2).max(100),
-  publicationDate: Joi.date(),
-  totalCopies: Joi.number().integer().min(1)
+  category: Joi.string(),
+  publicationDate: Joi.date().iso(),
+  totalCopies: Joi.number().integer().min(1),
 });
